72a2f00377fe2399b87058fcf2bfe0e4b6fd22f4
=JoaoFernandes <=joaomgomesf@gmail.com>
2019-05-30 22:10:16 +0100

    Criaçao do resource ball.

--- Client/resource1.h
+++ Client/resource1.h
@@ -3,18 +3,18 @@
 // Used by Client.rc
 //
 #define IDD_DIALOGLOGIN                 5
+#define IDB_BITMAP1                     104
 #define IDC_USERNAME                    1001
 #define IDC_SENDLOGIN                   1002
 #define IDC_COMMUNICATION               1003
 #define IDC_RADIOSHAREDMEMORY           1004
 #define IDC_RADIONAMEDPIPE              1005
-#define IDC_STATIC                      -1
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
