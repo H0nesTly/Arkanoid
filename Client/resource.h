1fb3030243aa5e0f75a942ad4765ef7b5650b14a
=JoaoFernandes <=joaomgomesf@gmail.com>
2019-06-05 21:39:00 +0100

    Como devia de estar

--- Client/resource.h
+++ Client/resource.h
@@ -2,6 +2,7 @@
 // Microsoft Visual C++ generated include file.
 // Used by Client.rc
 //
+#define IDD_DIALOGLOGIN                 5
 #define IDB_BITMAPBALL                  101
 #define IDB_BITMAPEXTRAHEALTH           102
 #define IDB_BITMAPLIFE                  104
@@ -9,14 +10,21 @@
 #define IDB_BITMAPSPEEDUP               106
 #define IDB_BITMAP1                     107
 #define IDB_BITMAPTRIPLE                107
+#define IDC_USERNAME                    1002
+#define IDC_SENDLOGIN                   1003
+#define IDC_COMMUNICATION               1004
+#define IDC_RADIOSHAREDMEMORY           1005
+#define IDC_RADIO2                      1006
+#define IDC_RADIONAMEDPIPE              1006
+#define IDC_STATIC                      -1
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
